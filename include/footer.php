d8d1995a5f03a7f1d1c0ed87d33fe9feae366e90
suranjan rai <suranjanthulungrai@gmail.com>
2020-05-03 15:09:50 +0545

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+<footer></footer>
+
+
+    <script>
+        AOS.init({
+            duration: 600,
+            delay: 100,
+
+        });
+    </script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Lemonada:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="img/logo.png">
+
+    <!-- for bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- /bootstrap -->
+
+    <!-- Aos -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!-- /Aos -->
+
+    <title>Websites</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
